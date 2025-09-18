50698696fa18e8e458af863b54ce993211809138
Radin <radinrafatpanah725@gmail.com>
2025-09-18 23:05:44 +0330

    lb-phone

---
+++ lb-phoneprop/fxmanifest.lua
@@ -0,0 +1,15 @@
+-- Decrypted by https://discord.gg/FYW52KjbAC
+-- Decrypted by https://discord.gg/FYW52KjbAC
+-- Decrypted by https://discord.gg/FYW52KjbAC
+-- Decrypted by https://discord.gg/FYW52KjbAC
+-- Decrypted by https://discord.gg/FYW52KjbAC
+fx_version "cerulean"
+game "gta5"
+
+author "LB"
+title "LB Phone Model"
+description "Phone prop for lb-phone"
+
+this_is_a_map "yes"
+
+data_file "DLC_ITYP_REQUEST" "lbphone_props.ytyp"
